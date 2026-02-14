1a597c46b9a2f2c0f9e2ea6fa9da64d61a9a0f6a
saycc <saymonhenrique.cassiano@gmail.com>
2026-02-13 22:38:28 -0300

    Adição de animação e scroll suave no header

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -21,4 +21,37 @@ document.addEventListener("DOMContentLoaded", () => {
         icon.classList.toggle("fa-bars");
         icon.classList.toggle("fa-xmark");
     });
+
+    // Jquery Scroll spy para adicionar classe active nos links de navegação
+    $(window).on('scroll', function() {
+        var scrollPos = $(window).scrollTop() + 100; // offset para considerar o header
+
+        // Remover active de todos
+        $('#navi_links .navi-item, #mobile_links .mobile-item').removeClass('active');
+
+        // Verificar cada link, ignorando #home
+        $('#navi_links .navi-item a, #mobile_links .mobile-item a').each(function() {
+            var currLink = $(this);
+            var href = currLink.attr("href");
+            if (href !== "#home") {
+                var refElement = $(href);
+                if (refElement.length && refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                    currLink.parent().addClass('active');
+                }
+            }
+        });
+    });
+
+    
+    // Smooth scroll para links de navegação
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $($(this).attr('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top - 15 // ajuste para o header
+            }, 1000);
+        }
+    }); 
+
 });
