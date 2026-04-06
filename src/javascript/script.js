850536bcbd8b86384f7fd3c0dea7491226e84890
saycc <saymonhenrique.cassiano@gmail.com>
2026-04-06 11:34:58 -0300

    Remoção do lightwidget e adição do slick-carousel

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -146,4 +146,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
     counters.forEach(counter => observer.observe(counter));
 
-}); 
+    // Configuração do Slick Carousel pego do codepen
+    $(document).ready(function(){
+            $('.slider').slick({
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                arrows: true,
+                dots: true,
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 2
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 1,
+                            arrows: false
+                        }
+                    }
+                ]
+            });
+        });
+});
